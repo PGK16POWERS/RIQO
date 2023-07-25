393d339800a9f454e6c8018c201c7f9e73956771
PGK16POWERS <136324051+PGK16POWERS@users.noreply.github.com>
2023-07-25 09:09:09 +0000

    hlonipa

--- server.js
+++ server.js
@@ -1,12 +1,11 @@
 const express = require("express");
 const app = express();
-const mysql = require("mysql2");
 const path = require("path");
 
 app.use(express.static("htmlFiles"));
 app.use(express.static("javascriptModules"));
 app.use(express.static("stylesfolder"));
-
+app.use(express.static("photos"))
 
 
 app.listen(2300, () => {
