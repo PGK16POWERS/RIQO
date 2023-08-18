8f03c2504f80559e263108accead516a69750143
PGK16POWERS <136324051+PGK16POWERS@users.noreply.github.com>
2023-08-18 10:24:38 +0200

    thats a rack

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const app = express();
+const ejs = require("ejs")
 const path = require("path");
 
 app.set("view engine","ejs");
@@ -10,16 +11,9 @@ app.use(express.static("stylesfolder"));
 app.use(express.static("photos"));
 
 app.get("/", (req,res) => {
-    res.status(200).sendFile(path.join(__dirname,"htmlfiles","index.html"));
+    res.render("index.ejs")
 });
 
-app.get("/styl.css", (req,res) => {
-    res.status(200).sendFile(path.join(__dirname,"stylesfolder","styl.css"));
-});
-
-app.get("/basicpagefunc.js", (req,res) => {
-    res.status(200).sendFile(path.join(__dirname,"javascriptModules","basicpagefunc.js"));
-});
 
 app.listen(2300, () => {
     console.log("Up it like Its cheese");
