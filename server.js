386821b80ebdfc128e9511e283d95a9f5d59635b
Liberia (Kaiberius) <136324051+PGK16POWERS@users.noreply.github.com>
2023-08-19 12:44:09 +0000

    solbased

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
 const express = require("express");
 const app = express();
 const ejs = require("ejs")
-const path = require("path");
+
 
 app.set("view engine","ejs");
 app.use(express.static("views"))
 app.use(express.static("htmlFiles"));
-app.use(express.static("javascriptModules"));
+app.use(express.static("javascriptmods"));
 app.use(express.static("stylesfolder"));
 app.use(express.static("photos"));
 
